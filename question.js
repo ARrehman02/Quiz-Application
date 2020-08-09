8d2a37d5c1381c11388769f6d1e193d600258043
ARrehman02 <arrehman6634583@gamil.com>
2020-08-09 19:35:17 +0500

    Quiz App Assignment

---
+++ question.js
@@ -0,0 +1,29 @@
+// Array of Objects
+
+const quiz = [
+    {
+        q:'On Which date of ramzan  Pakistan gots its Independence ?',
+        options:['21','23','25','27'],
+        answer:3
+    },
+    {
+        q:'What Pakistan Means ?',
+        options:['land of the pure','land of Flowers','The land of Pakistani','Diverse'],
+        answer:0
+    },
+    {
+        q:'Pakistan is located in the',
+        options:['East Asia','West Asia','South Asia','Central Asia'],
+        answer:2
+    },
+    {
+        q:'Which from the following countries is not bordered by Pakistan?',
+        options:['India','Afghanistan','China','Bangladesh'],
+        answer:3
+    },
+    {
+        q:'K2 the second highest mountain in the world is located along the border of Pakistan and',
+        options:['Iran','China','Afghanistan','India'],
+        answer:1
+    }
+]
